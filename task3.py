1fdc2c12b8e16ad411de9952f9451a62dcc2d994
ConsciousCode <conscious.code@gmail.com>
2026-05-01 17:14:54 -0600

    task3 type annotations

--- task3.py
+++ task3.py
@@ -6,10 +6,12 @@ Students: Jackie Javier, Pranitha Achanta, Robert McDaniels
 import numpy as np
 import random
 
+from task2 import Action, Point
+
 # Agent generalized for both SARSA and Q-Learning
 class Agent:
   # Initialize with dimensions for building the Q-table + epsilon
-  def __init__(self, rows, cols, actions, epsilon=0.1):
+  def __init__(self, rows: int, cols: int, actions: list[Action], epsilon: float=0.1):
     self.rows = rows
     self.cols = cols
     self.actions = actions
@@ -23,24 +25,20 @@ class Agent:
     self.action_to_index = {a: i for i, a in enumerate(actions)}
 
   # Exploration vs exploitation choice
-  def choose_action(self, state):
+  def choose_action(self, state: Point):
     if random.random() < self.epsilon:
       return random.choice(self.actions)
 
-    r, c = state
-    return self.actions[np.argmax(self.Q[r, c])]
+    return self.actions[np.argmax(self.Q[state])]
 
   # Used in task6.py when finding accuracy
-  def best_action(self, state):
-    r, c = state
-    return self.actions[np.argmax(self.Q[r, c])]
+  def best_action(self, state: Point):
+    return self.actions[np.argmax(self.Q[state])]
 
   # Getter
-  def get_q(self, state, action):
-    r, c = state
-    return self.Q[r, c, self.action_to_index[action]]
+  def get_q(self, state: Point, action: Action):
+    return self.Q[*state, self.action_to_index[action]]
 
   # Setter
-  def set_q(self, state, action, value):
-    r, c = state
-    self.Q[r, c, self.action_to_index[action]] = value
+  def set_q(self, state: Point, action: Action, value: int):
+    self.Q[*state, self.action_to_index[action]] = value
