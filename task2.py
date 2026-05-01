8b630f536fecb979a64319c803ea4f7561dce143
ConsciousCode <conscious.code@gmail.com>
2026-05-01 17:12:05 -0600

    task2 cleanup

--- task2.py
+++ task2.py
@@ -3,14 +3,27 @@ Intro to Machine Learning Final
 Encompasses the solution to Task 2.
 Students: Jackie Javier, Pranitha Achanta, Robert McDaniels
 """
+from typing import Literal, NamedTuple
 import numpy as np
 import matplotlib.pyplot as plt
 from task1 import get_abstract_map
 
+type Action = Literal['up', 'down', 'left', 'right']
+type Reward = int
+type Strategy = Literal["S1", "S2"]
+
+class Point(NamedTuple):
+  row: int
+  col: int
+
+# For S2, closer to target reward calculation
+def manhattan(s1, s2):
+  return abs(s1.row - s2.row) + abs(s1.col - s2.col)
+  
 # The Environment Class
 class GridEnvironment:
   # Constructor that takes name (for plotting purposes), the abstracted map, target pos, and S1/S2
-  def __init__(self, name, grid_map, target, reward_strategy="S1"):
+  def __init__(self, name: int, grid_map: np.ndarray, target: Point, reward_strategy: Strategy="S1"):
     """
     name: 1, 2, 3, or 4
     grid_map: 2D numpy array (1 = free, 0 = obstacle)
@@ -24,19 +37,15 @@ class GridEnvironment:
     self.strategy = reward_strategy
 
     # Action space
-    self.actions = {
+    self.actions: dict[Action, tuple[int, int]] = {
       "up": (-1, 0),
       "down": (1, 0),
       "left": (0, -1),
       "right": (0, 1)
     }
 
-  # For S2, closer to target reward calculation
-  def manhattan(self, s1, s2):
-    return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1])
-
   # Agent interaction: take state and action, return next state and immediate reward
-  def step(self, state, action):
+  def step(self, state: Point, action: Action) -> tuple[Point, Reward]:
     """
     state: (row, col)
     action: "up", "down", "left", "right"
@@ -44,10 +53,10 @@ class GridEnvironment:
     """
 
     dr, dc = self.actions[action]
-    next_state = (state[0] + dr, state[1] + dc)
+    next_state = Point(state.row + dr, state.col + dc)
 
     # Boundary check
-    if not (0 <= next_state[0] < self.rows and 0 <= next_state[1] < self.cols):
+    if not (0 <= next_state.row < self.rows and 0 <= next_state.col < self.cols):
       return state, -100
 
     # Obstacle check
@@ -62,35 +71,35 @@ class GridEnvironment:
     if self.strategy == "S1":
       return next_state, 0
 
+    old_dist = manhattan(state, self.target)
+    new_dist = manhattan(next_state, self.target)
+
+    reward = np.sign(old_dist - new_dist)
+    if new_dist < old_dist:
+      reward = +1
+    elif new_dist > old_dist:
+      reward = -1
     else:
-      old_dist = self.manhattan(state, self.target)
-      new_dist = self.manhattan(next_state, self.target)
+      reward = 0
 
-      if new_dist < old_dist:
-        reward = +1
-      elif new_dist > old_dist:
-        reward = -1
-      else:
-        reward = 0
-
-      return next_state, reward
+    return next_state, reward
 
   # Plot the map abstraction and target position
   def plot(self):
     plt.imshow(self.map, cmap="gray", origin="lower")
-    plt.title("Grid Environment " + str(self.name))
+    plt.title(f"Grid Environment {self.name}")
 
     # Mark target
-    plt.scatter(self.target[1], self.target[0], c='red', marker='X')
+    plt.scatter(self.target.col, self.target.row, c='red', marker='X')
 
     plt.gca().invert_yaxis()
-    plt.savefig(str(self.name) + ".png")
+    plt.savefig(f"{self.name}.png")
 
 # Test
 def main():
   grid = get_abstract_map(2, 40, 40)
-  env = GridEnvironment(2, grid, target=(39, 39), reward_strategy="S2")
-  state = (38, 38)
+  env = GridEnvironment(2, grid, target=Point(39, 39), reward_strategy="S2")
+  state = Point(38, 38)
   next_state, reward = env.step(state, "right")
   print("Next:", next_state, "Reward:", reward)
   env.plot()
