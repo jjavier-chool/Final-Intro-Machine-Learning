51e92cca22582afe353ea499bc702df322faae2a
jjavier-chool <jjavier1@unm.edu>
2026-05-06 10:41:40 -0600

    Fixing Q-learning

--- task5.py
+++ task5.py
@@ -21,11 +21,10 @@ def q_learning(
   ):
   for ep in tqdm(range(episodes), desc="Q-Learning Training"):
     state = start_state
-    action = agent.choose_action(state)
 
     for _ in range(max_steps):
+      action = agent.choose_action(state)
       next_state, reward = env.step(state, action)
-      next_action = agent.choose_action(next_state)
 
       # Same as SARSA except this section, want max
       q = agent.get_q(state, action)
@@ -34,7 +33,7 @@ def q_learning(
       new_q = q + alpha * (reward + gamma * q_next_max - q)
       agent.set_q(state, action, new_q)
 
-      state, action = next_state, next_action
+      state = next_state
 
       if state == env.target:
         break
